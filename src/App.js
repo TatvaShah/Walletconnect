866507bfc7547e2a17e6e7cc2fe46e09a9e400f7
Shazad Shiraz <shazadshiraz97@gmail.com>
2024-08-02 22:26:26 +0400

    rainbow kit integration

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import { bsc, bscTestnet } from 'wagmi/chains'
-import Web3Connect from './Web3Connect'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-const contractAddress = '0x93749e69560efe1ad6661903e47df538492c50a4';
-const chains = [bsc, bscTestnet]
-const projectId = 'a6418a1e28c8c7ec2ef9fb805e53b33b'
+import Web3Connect from "./Web3Connect";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { bsc, bscTestnet } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const contractAddress = "0x93749e69560efe1ad6661903e47df538492c50a4";
+const chains = [bsc, bscTestnet];
+const projectId = "c1c90419ca0aeebdd454ed9c43d57657";
+
+const config = getDefaultConfig({
+  appName: "EVDC",
+  projectId,
+  chains,
+  ssr: false, // If your dApp uses server side rendering (SSR)
+});
 
 const queryClient = new QueryClient();
-const metadata = {
-  name: 'Evdc',
-  description: 'Evdc Network',
-  url: 'https://evdc.network', // origin must match your domain & subdomain
-  icons: ['https://evdc.network/cdn/shop/files/EVDC-01_26edacf5-f039-4b95-8e61-0396beba5b32.png']
-}
-const config = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata,
-})
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
 
 export function Web3ModalProvider({ children }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
-  )
+  );
 }
 
 function App() {
-  const searchParams = new URLSearchParams(document.location.search)
-  const amount = searchParams.get('amount');
+  const searchParams = new URLSearchParams(document.location.search);
+  const amount = searchParams.get("amount");
   const [tokenData, setTokenData] = useState(false);
   const fetchTokenPrice = async () => {
     try {
@@ -48,47 +42,43 @@ function App() {
         `https://api.coingecko.com/api/v3/simple/token_price/binance-smart-chain?contract_addresses=${contractAddress}&vs_currencies=usd`
       );
       const priceInUSD = response?.data[contractAddress.toLowerCase()]?.usd;
-      if (priceInUSD && typeof priceInUSD === 'number') {
-        setTokenData(priceInUSD)
+      if (priceInUSD && typeof priceInUSD === "number") {
+        setTokenData(priceInUSD);
       }
     } catch (error) {
-      console.error('Error fetching token data:', error);
+      console.error("Error fetching token data:", error);
     }
   };
 
-
   useEffect(() => {
-    fetchTokenPrice()
-  }, [])
+    fetchTokenPrice();
+  }, []);
 
   const calculatePrice = () => {
     if (tokenData) {
       const amountInNumber = Number(amount);
       const evdcToPurchase = (amountInNumber / tokenData).toString();
-      console.log(evdcToPurchase, "logm!")
+      console.log(evdcToPurchase, "logm!");
       return evdcToPurchase;
     }
-
-  }
+  };
   return (
     <>
       <Web3ModalProvider>
-        {
-          amount && tokenData ?
-            <Web3Connect amount={tokenData ? calculatePrice(amount) : false} /> :
-            !amount ? 
-            <div className='something-went-wrong'>
-              <p>something went wrong!</p>
-            </div> :
-              <div className='something-went-wrong'>
-              <p>please wait</p>
-            </div>
-
-        }
-
+        {amount && tokenData ? (
+          <Web3Connect amount={tokenData ? calculatePrice(amount) : false} />
+        ) : !amount ? (
+          <div className="something-went-wrong">
+            <p>something went wrong!</p>
+          </div>
+        ) : (
+          <div className="something-went-wrong">
+            <p>please wait</p>
+          </div>
+        )}
       </Web3ModalProvider>
     </>
-  )
+  );
 }
 
 export default App;
--- src/Web3Connect.js
+++ src/Web3Connect.js
@@ -1,129 +1,205 @@
-import React, { useEffect, useState } from 'react'
-import { useAccount, useBalance, useWriteContract, useToken, useWalletClient } from 'wagmi'
-import contractAbi from './contract-abi'
-import Image from './image'
-import BN from 'bignumber.js'
-import Modal from 'react-modal';
-const contractAddress = '0x93749e69560efe1ad6661903e47df538492c50a4';
-const walletAddress = '0x93467f0F9a09b5478B0E2ECdA979045dda53750b';
+import React, { useEffect, useState } from "react";
+import {
+  useAccount,
+  useBalance,
+  useWriteContract,
+  useToken,
+  useWalletClient,
+  useDisconnect,
+} from "wagmi";
+import contractAbi from "./contract-abi";
+import Image from "./image";
+import BN from "bignumber.js";
+import Modal from "react-modal";
+import { WalletButton } from "@rainbow-me/rainbowkit";
+
+const contractAddress = "0x93749e69560efe1ad6661903e47df538492c50a4";
+const walletAddress = "0x93467f0F9a09b5478B0E2ECdA979045dda53750b";
 
 const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-    },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
 };
 
-const Web3Connect = ({
-    amount
-}) => {
-    console.log(amount, typeof amount)
-    const { isConnected } = useAccount()
-    const { data: walletData } = useWalletClient()
-    const { data: balanceData } = useBalance({
-        address: walletData?.account?.address,
-        token: contractAddress,
-        watch: true,
-    });
-    const [loader, setLoader] = useState(false);
-    const [showModal, setShowModal] = useState(false);
+const Web3Connect = ({ amount }) => {
+  console.log(amount, typeof amount);
+  const { disconnect: disconnectWallet } = useDisconnect();
+  const { isConnected, address: connectedWalletAddress } = useAccount();
+  const { data: walletData } = useWalletClient();
+  const { data: balanceData } = useBalance({
+    address: walletData?.account?.address,
+    token: contractAddress,
+    watch: true,
+  });
+  const [loader, setLoader] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const tokenDecimals = new BN(9);
-    const tokenAmountToTransfer = new BN(Math.ceil(Number(amount)));
-    const calculatedTransferValue = tokenAmountToTransfer.multipliedBy(new
-        BN(10).pow(tokenDecimals));
-    const { data: hash, error, isPending, writeContract } = useWriteContract()
-    const handleOnTransaction = () => {
-        if (!isPending) {
-            setShowModal(false)
-            setLoader(true);
-            writeContract({
-                address: contractAddress,
-                abi: contractAbi,
-                functionName: 'transfer',
-                args: [walletAddress, calculatedTransferValue],
+  const tokenDecimals = new BN(9);
+  const tokenAmountToTransfer = new BN(Math.ceil(Number(amount)));
+  const calculatedTransferValue = tokenAmountToTransfer.multipliedBy(
+    new BN(10).pow(tokenDecimals)
+  );
+  const { data: hash, error, isPending, writeContract } = useWriteContract();
+  const handleOnTransaction = () => {
+    if (!isPending) {
+      setShowModal(false);
+      setLoader(true);
+      writeContract({
+        address: contractAddress,
+        abi: contractAbi,
+        functionName: "transfer",
+        args: [walletAddress, calculatedTransferValue],
+      });
+    }
+  };
+  const OnCancelTransaction = () => {
+    setLoader(false);
+    setShowModal(false);
+  };
+
+  useEffect(() => {
+    if (hash) {
+      const transactionHash = hash;
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage(
+          JSON.stringify({
+            error: false,
+            hash: transactionHash,
+            amount: Number(Number(amount).toFixed(2)),
+          })
+        );
+      }
+
+      setLoader(false);
+    }
+  }, [hash]);
+
+  useEffect(() => {
+    if (error) {
+      console.log(error, "error");
+      const errorMessage = error.message.split("Contract Call:");
+      if (errorMessage.length > 0) {
+        let errorMessageToShow = errorMessage[0];
+        if (
+          errorMessageToShow.toLowerCase()?.includes("subtraction overflow")
+        ) {
+          errorMessageToShow = `Your EVDC balance is insufficient. EVDC tokens needed : ${Number(
+            amount
+          ).toFixed(2)} Your EVDC balance : ${Number(
+            balanceData?.formatted
+          ).toFixed(2)}`;
+        } else {
+          errorMessageToShow = `Something went wrong!`;
+        }
+        if (window.ReactNativeWebView) {
+          window.ReactNativeWebView.postMessage(
+            JSON.stringify({
+              error: true,
+              message: errorMessageToShow,
             })
+          );
+        } else {
+          alert(errorMessageToShow);
         }
+      } else {
+        alert("Something went wrong!");
+      }
+      setLoader(false);
     }
-    const OnCancelTransaction = () => {
-        setLoader(false);
-        setShowModal(false);
+  }, [error]);
+
+  useEffect(() => {
+    if (isConnected && !isPending && amount) {
+      setLoader(true);
+      setTimeout(() => {
+        setShowModal(true);
+        // handleOnTransaction()
+      }, 2000);
     }
+  }, [isConnected, isPending, amount]);
 
-    useEffect(() => {
-        if (hash) {
-            const transactionHash = hash;
-            if (window.ReactNativeWebView) {
-
-                window.ReactNativeWebView.postMessage(JSON.stringify({
-                    error: false, hash:
-                        transactionHash, amount: Number(Number(amount).toFixed(2))
-                }));
-            }
-
-            setLoader(false);
-        }
-    }, [hash])
-
-    useEffect(() => {
-        if (error) {
-            console.log(error , "error")
-            const errorMessage = error.message.split('Contract Call:');
-            if (errorMessage.length > 0) {
-                let errorMessageToShow = errorMessage[0];
-                if (errorMessageToShow.toLowerCase()?.includes("subtraction overflow")) {
-                    errorMessageToShow = `Your EVDC balance is insufficient. EVDC tokens needed : ${Number(amount).toFixed(2)} Your EVDC balance : ${Number(balanceData?.formatted).toFixed(2)}`;
-                } else {
-                    errorMessageToShow = `Something went wrong!`
-                }
-                if (window.ReactNativeWebView) {
-                    window.ReactNativeWebView.postMessage(JSON.stringify({
-                        error: true, message: errorMessageToShow
-                    }));
-                } else {
-                    alert(errorMessageToShow)
-                }
-            } else {
-                alert("Something went wrong!")
-            }
-            setLoader(false);
-        }
-    }, [error])
-
-    useEffect(() => {
-        if (isConnected && !isPending && amount) {
-            setLoader(true);
-            setTimeout(() => {
-                setShowModal(true)
-                // handleOnTransaction()
-            }, 2000)
-        }
-    }, [isConnected, isPending, amount])
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', flex: 1, marginTop: 20, gap: 20 }}>
-            {
-                <Modal style={customStyles} isOpen={showModal}>
-                    <h2>Confirmation</h2>
-                    <p>Are you sure you want to proceed with the transaction of EVDC token {Number(amount).toFixed(2)}</p>
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: 20 }}>
-                        <button className='btn-class-2' onClick={handleOnTransaction} ><p className='btn-txt'>{`Perform Transaction`}</p></button>
-                        <button className='btn-class-3' onClick={OnCancelTransaction} ><p className='btn-txt'>{`Cancel`}</p></button>
-                    </div>
-                </Modal>
-            }
-            <img src={Image.EVDC_LOGO}></img>
-            <w3m-button />
-            {
-                isConnected && <button disabled={loader} className='btn-class-2' onClick={handleOnTransaction} ><p className='btn-txt'>{loader ? `Loading...` : `Perform Transaction`}</p></button>
-            }
-        </div>
-
-    )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        flex: 1,
+        marginTop: 20,
+        gap: 20,
+      }}
+    >
+      {
+        <Modal style={customStyles} isOpen={showModal}>
+          <h2>Confirmation</h2>
+          <p>
+            Are you sure you want to proceed with the transaction of EVDC token{" "}
+            {Number(amount).toFixed(2)}
+          </p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              gap: 20,
+            }}
+          >
+            <button className="btn-class-2" onClick={handleOnTransaction}>
+              <p className="btn-txt">{`Perform Transaction`}</p>
+            </button>
+            <button className="btn-class-3" onClick={OnCancelTransaction}>
+              <p className="btn-txt">{`Cancel`}</p>
+            </button>
+          </div>
+        </Modal>
+      }
+      <img src={Image.EVDC_LOGO}></img>
+      {!isConnected ? (
+        <WalletButton.Custom wallet="walletconnect">
+          {({ ready, connect }) => {
+            return (
+              <button
+                disabled={loader}
+                className="btn-class-2"
+                onClick={connect}
+              >
+                <p className="btn-txt">Connect Wallet</p>
+              </button>
+            );
+          }}
+        </WalletButton.Custom>
+      ) : (
+        <>
+          <p style={{marginLeft: 10}}>Wallet Address: {connectedWalletAddress}</p>
+          <div style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+            <button
+              disabled={loader}
+              className="btn-class-2"
+              onClick={disconnectWallet}
+            >
+              <p className="btn-txt">Disconnect</p>
+            </button>
+            <button
+              disabled={loader}
+              className="btn-class-2"
+              onClick={handleOnTransaction}
+            >
+              <p className="btn-txt">
+                {loader ? `Loading...` : `Perform Transaction`}
+              </p>
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Web3Connect;
